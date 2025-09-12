a3e567a4d368d3a1802a9e2fd41277ed8bd53428
Егор Москалев <moskalevegor310@gmail.com>
2025-09-12 22:11:23 +0500

    Task7

---
+++ Tyuiu.SpirinIS.Sprint0.Task7.V0Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.DatskiyDE.Sprint0.Task7.V0.Lib;
+
+namespace Tyuiu.DatskiyDE.Sprint0.Task7.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArraysValid()
+        {
+            int[] num1 = new int[] { 1, 1, 1, 1, 1 };
+            int[] num2 = new int[] { 1, 1, 1, 1, 1 };
+            int[] num3 = new int[] { 2, 2, 2, 2, 2 };
+            int[] res = DataService.AdditionArrays(num1, num2);
+            CollectionAssert.AreEqual(num3, res);
+        }
+    }
+}
---
+++ Tyuiu.SpirinIS.Sprint0.Task7.V0Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SpirinIS.Sprint0.Task7.V0Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SpirinIS.Sprint0.Task7.V0Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b95348fa-9c9c-449c-98dc-91e71c76cddb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
